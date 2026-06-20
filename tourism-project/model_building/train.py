9d72df29223f1779656401754e33d5ede1f49eba
Kalyan412 <pavulurichakri@gmail.com>
2026-06-20 17:24:33 +0000

    third commit

--- tourism-project/model_building/train.py
+++ tourism-project/model_building/train.py
@@ -23,10 +23,10 @@ mlflow.set_experiment("mlops-training-experiment")
 api = HfApi()
 
 
-Xtrain_path = "hf://datasets/praneeth232/machine-failure-prediction/Xtrain.csv"
-Xtest_path = "hf://datasets/praneeth232/machine-failure-prediction/Xtest.csv"
-ytrain_path = "hf://datasets/praneeth232/machine-failure-prediction/ytrain.csv"
-ytest_path = "hf://datasets/praneeth232/machine-failure-prediction/ytest.csv"
+Xtrain_path = "hf://datasets/Kalyanac/tourism-project/Xtrain.csv"
+Xtest_path = "hf://datasets/Kalyanac/tourism-project/Xtest.csv"
+ytrain_path = "hf://datasets/Kalyanac/tourism-project/ytrain.csv"
+ytest_path = "hf://datasets/Kalyanac/tourism-project/ytest.csv"
 
 Xtrain = pd.read_csv(Xtrain_path)
 Xtest = pd.read_csv(Xtest_path)
